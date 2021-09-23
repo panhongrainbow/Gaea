901fc74c4a7b97702010d27e0558078f2f64b520
PanHongRainbow <panhongrainbow@gmail.com>
2021-09-23 12:33:31 +0800

    test: mock insert (4/4)

--- backend/mock_to_novel.go
+++ backend/mock_to_novel.go
@@ -50,8 +50,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 618120042:
+		if err := fdb.MockDataInDB[0].InsertSecondNovelResult(); err != nil { // 写入第二本小说到数据库 水浒传
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 514659115:
 		if err := fdb.MockDataInDB[1].InsertThirdNovelResult(); err != nil { // 写入第三本小说到数据库 西游记
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 4273731942:
+		if err := fdb.MockDataInDB[0].InsertFourthNovelResult(); err != nil { // 写入第四本小说到数据库 红楼梦
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
@@ -62,8 +74,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 1926088204:
+		if err := fdb.MockDataInDB[0].InsertSixthNovelResult(); err != nil { // 写入第六本小说到数据库 儒林外史
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 1572904758:
 		if err := fdb.MockDataInDB[1].InsertSeventhNovelResult(); err != nil { // 写入第七本小说到数据库 初刻拍案惊奇
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 1708424148:
+		if err := fdb.MockDataInDB[0].InsertEighthNovelResult(); err != nil { // 写入第八本小说到数据库 二刻拍案惊奇
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
@@ -74,8 +98,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 3303343655:
+		if err := fdb.MockDataInDB[0].InsertTenthNovelResult(); err != nil { // 写入第十本小说到数据库 镜花缘
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 3599615497:
 		if err := fdb.MockDataInDB[1].InsertEleventhNovelResult(); err != nil { // 写入第十一本小说到数据库 喻世明言
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 600352469:
+		if err := fdb.MockDataInDB[0].InsertTwelfthNovelResult(); err != nil { // 写入第十二本小说到数据库 说岳全传
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
@@ -86,8 +122,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 1226676578:
+		if err := fdb.MockDataInDB[0].InsertFourteenthNovelResult(); err != nil { // 写入第十四本小说到数据库 说唐
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 56203336:
 		if err := fdb.MockDataInDB[1].InsertFifteenthNovelResult(); err != nil { // 写入第十五本小说到数据库 七侠五义
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 3585696861:
+		if err := fdb.MockDataInDB[0].InsertSixteenthNovelResult(); err != nil { // 写入第十六本小说到数据库 施公案
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
@@ -98,8 +146,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 1792929480:
+		if err := fdb.MockDataInDB[0].InsertEighteenthNovelResult(); err != nil { // 写入第十八本小说到数据库 歧路灯
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 398747927:
 		if err := fdb.MockDataInDB[1].InsertNineteenthNovelResult(); err != nil { // 写入第十九本小说到数据库 老残游记
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 1187323765:
+		if err := fdb.MockDataInDB[0].InsertTwentiethNovelResult(); err != nil { // 写入第二十本小说到数据库 二十年目睹之怪现状
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
@@ -110,8 +170,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 2032678570:
+		if err := fdb.MockDataInDB[0].InsertTwentySecondNovelResult(); err != nil { // 写入第二十二本小说到数据库 官场现形记
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 2614046017:
 		if err := fdb.MockDataInDB[1].InsertTwentyThirdNovelResult(); err != nil { // 写入第二十三本小说到数据库 觉世名言十二楼
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 2457093340:
+		if err := fdb.MockDataInDB[0].InsertTwentyFourthNovelResult(); err != nil { // 写入第二十四本小说到数据库 无声戏
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
@@ -122,8 +194,20 @@ func (fdb *fakeDB) switchNovelResult(key uint32) (*mysql.Result, error) {
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
 		return mysql.MakeNovelEmptyResult()
+	case 4020693348:
+		if err := fdb.MockDataInDB[0].InsertTwentySixthNovelResult(); err != nil { // 写入第二十六本小说到数据库 浮生六记
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
 	case 2745523730:
 		if err := fdb.MockDataInDB[1].InsertTwentySeventhNovelResult(); err != nil { // 写入第二十七本小说到数据库 野叟曝言
+			return nil, err
+		}
+		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
+		return mysql.MakeNovelEmptyResult()
+	case 1776512190:
+		if err := fdb.MockDataInDB[0].InsertTwentyEighthNovelResult(); err != nil { // 写入第二十八本小说到数据库 九尾龟
 			return nil, err
 		}
 		fmt.Printf("\u001B[35m 命中数据库所对应的 Key: %d\n", key)
