8c09954dd5de2445792bdb514bbb8b39fab1af44
PanHongRainbow <panhongrainbow@gmail.com>
2021-11-18 08:07:17 +0800

    feature: test router hash and mod

--- proxy/router/router_test.go
+++ proxy/router/router_test.go
@@ -6,8 +6,14 @@ import (
 	"testing"
 )
 
-// TestNovelRouter 函式 🧚 是用来测试小說資料庫的路由
-func TestNovelRouter(t *testing.T) {
+// 参考文件 https://github.com/XiaoMi/Gaea/blob/master/docs/shard-example.md#gaea_kingshard_mod
+
+// TestNovelRouterHashType 函式 🧚 是用来测试小說数据库的 hash 路由
+func TestNovelRouterHashType(t *testing.T) {
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例1
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001
 
 	// 再建立 路由规则 设定模组
 	cfgRouter := models.Shard{
@@ -21,19 +27,89 @@ func TestNovelRouter(t *testing.T) {
 		TableRowLimit: 0,
 	}
 
-	// 直接建立路由规则
+	// 直接产生路由规则
 	rule, err := parseRule(&cfgRouter)
 	require.Equal(t, err, nil)
 
-	// 直接检查路由的设定值
+	// 检查目前的路由设定值
 	require.Equal(t, rule.ruleType, "hash")
 	require.Equal(t, rule.db, "novel")
 	require.Equal(t, rule.table, "book")
 	require.Equal(t, rule.slices, []string{"slice-0", "slice-1"})
 	require.Equal(t, rule.shard.(*HashShard).ShardNum, 2)
 	require.Equal(t, rule.shardingColumn, "bookid")
+
+	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是傳輸函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 為參數，產生輸出得來的
 	require.Equal(t, rule.subTableIndexes, []int{0, 1})
 	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1})
+
 	require.Equal(t, len(rule.mycatDatabases), 0)
 	require.Equal(t, len(rule.mycatDatabaseToTableIndexMap), 0)
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例2
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001 Book_0002
+
+	// 修改 路由规则 设定模组
+	cfgRouter.Locations = []int{1, 2}
+
+	// 直接产生路由规则
+	rule, err = parseRule(&cfgRouter)
+	require.Equal(t, err, nil)
+
+	// 检查目前的路由设定值
+	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1, 2: 1})
+}
+
+// TestNovelRouterModType 函式 🧚 是用来测试小說数据库的 mod 路由
+func TestNovelRouterModType(t *testing.T) {
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例1
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001
+
+	// 再建立 路由规则 设定模组
+	cfgRouter := models.Shard{
+		DB:            "novel",
+		Table:         "Book",
+		ParentTable:   "",
+		Type:          "mod",
+		Key:           "BookID",
+		Locations:     []int{1, 1},
+		Slices:        []string{"slice-0", "slice-1"},
+		TableRowLimit: 0,
+	}
+
+	// 直接产生路由规则
+	rule, err := parseRule(&cfgRouter)
+	require.Equal(t, err, nil)
+
+	// 检查目前的路由设定值
+	require.Equal(t, rule.ruleType, "mod")
+	require.Equal(t, rule.db, "novel")
+	require.Equal(t, rule.table, "book")
+	require.Equal(t, rule.slices, []string{"slice-0", "slice-1"})
+	require.Equal(t, rule.shard.(*ModShard).ShardNum, 2)
+	require.Equal(t, rule.shardingColumn, "bookid")
+
+	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是傳輸函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 為參數，產生輸出得來的
+	require.Equal(t, rule.subTableIndexes, []int{0, 1})
+	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1})
+
+	require.Equal(t, len(rule.mycatDatabases), 0)
+	require.Equal(t, len(rule.mycatDatabaseToTableIndexMap), 0)
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例2
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001 Book_0002
+
+	// 修改 路由规则 设定模组
+	cfgRouter.Locations = []int{1, 2}
+
+	// 直接产生路由规则
+	rule, err = parseRule(&cfgRouter)
+	require.Equal(t, err, nil)
+
+	// 检查目前的路由设定值
+	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1, 2: 1})
 }
