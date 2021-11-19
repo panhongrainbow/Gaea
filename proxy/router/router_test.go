629c5c9cc0864f9f4b5c57e0a12294fe0a256c0d
PanHongRainbow <panhongrainbow@gmail.com>
2021-11-19 09:56:16 +0800

    feature: test router range

--- proxy/router/router_test.go
+++ proxy/router/router_test.go
@@ -39,7 +39,7 @@ func TestNovelRouterHashType(t *testing.T) {
 	require.Equal(t, rule.shard.(*HashShard).ShardNum, 2)
 	require.Equal(t, rule.shardingColumn, "bookid")
 
-	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是傳輸函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 為參數，產生輸出得來的
+	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是传输函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 为参数，产生输出得来的
 	require.Equal(t, rule.subTableIndexes, []int{0, 1})
 	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1})
 
@@ -92,7 +92,7 @@ func TestNovelRouterModType(t *testing.T) {
 	require.Equal(t, rule.shard.(*ModShard).ShardNum, 2)
 	require.Equal(t, rule.shardingColumn, "bookid")
 
-	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是傳輸函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 為參數，產生輸出得來的
+	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是传输函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 为参数，产生输出得来的
 	require.Equal(t, rule.subTableIndexes, []int{0, 1})
 	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1})
 
@@ -112,4 +112,74 @@ func TestNovelRouterModType(t *testing.T) {
 
 	// 检查目前的路由设定值
 	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1, 2: 1})
+}
+
+// TestNovelRouterRangeType 函式 🧚 是用来测试小說数据库的 range 路由
+func TestNovelRouterRangeType(t *testing.T) {
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例1
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001
+
+	// 再建立 路由规则 设定模组
+	cfgRouter := models.Shard{
+		DB:            "novel",
+		Table:         "Book",
+		ParentTable:   "",
+		Type:          "range",
+		Key:           "BookID",
+		Locations:     []int{1, 1},
+		Slices:        []string{"slice-0", "slice-1"},
+		TableRowLimit: 3, // Book_ID 0 至 2  放在第一个切片，Book_ID 3 至 5  放在第二个切片，Book_ID 6 至 8  放在第三个切片，以前类推
+	}
+
+	// 直接产生路由规则
+	rule, err := parseRule(&cfgRouter)
+	require.Equal(t, err, nil)
+
+	// 检查目前的路由设定值
+	require.Equal(t, rule.ruleType, "range")
+	require.Equal(t, rule.db, "novel")
+	require.Equal(t, rule.table, "book")
+	require.Equal(t, rule.slices, []string{"slice-0", "slice-1"})
+
+	// 观察分表是否是依照主键 BookID
+	require.Equal(t, rule.shardingColumn, "bookid")
+
+	// 检查在切片中分表编号的上下界限的范围
+	require.Equal(t, len(rule.shard.(*NumRangeShard).Shards), 2)
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[0].Start, int64(0))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[0].End, int64(3))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[1].Start, int64(3))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[1].End, int64(6))
+
+	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是传输函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 为参数，产生输出得来的
+	require.Equal(t, rule.subTableIndexes, []int{0, 1})
+	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1})
+
+	require.Equal(t, len(rule.mycatDatabases), 0)
+	require.Equal(t, len(rule.mycatDatabaseToTableIndexMap), 0)
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例2
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001 Book_0002
+
+	// 修改 路由规则 设定模组
+	cfgRouter.Locations = []int{1, 2}
+
+	// 直接产生路由规则
+	rule, err = parseRule(&cfgRouter)
+	require.Equal(t, err, nil)
+
+	// 检查目前的路由设定值
+	require.Equal(t, rule.tableToSlice, map[int]int{0: 0, 1: 1, 2: 1})
+
+	// 检查在切片中分表编号的上下界限的范围
+	require.Equal(t, len(rule.shard.(*NumRangeShard).Shards), 3)
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[0].Start, int64(0))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[0].End, int64(3))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[1].Start, int64(3))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[1].End, int64(6))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[2].Start, int64(6))
+	require.Equal(t, rule.shard.(*NumRangeShard).Shards[2].End, int64(9))
 }
