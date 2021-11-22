5e1647d8fef06fb665ad59137e3ff432075ae5bd
PanHongRainbow <panhongrainbow@gmail.com>
2021-11-22 13:17:40 +0800

    feature: test router month

--- proxy/router/router_test.go
+++ proxy/router/router_test.go
@@ -200,7 +200,7 @@ func TestNovelRouterModDateYear(t *testing.T) {
 		Key:         "Publish",
 		// Locations:     []int{1, 1}, // 路由规则模式 date_year 不使用 Locations
 		Slices:        []string{"slice-0", "slice-1"},
-		DateRange:     []string{"1500-1600", "1601-1700"}, // 路由规则模式 date_year 使用 DateRange
+		DateRange:     []string{"1500-1600", "1601-1700"}, // 路由规则模式 DateRange 使用 date_year
 		TableRowLimit: 0,
 	}
 
@@ -213,14 +213,58 @@ func TestNovelRouterModDateYear(t *testing.T) {
 	require.Equal(t, rule.db, "novel")
 	require.Equal(t, rule.table, "book")
 	require.Equal(t, rule.slices, []string{"slice-0", "slice-1"})
-	require.Equal(t, rule.shard.(*DateYearShard), rule.shard.(*DateYearShard))
+	// require.Equal(t, rule.shard.(*DateYearShard), rule.shard.(*DateYearShard))
 	require.Equal(t, rule.shardingColumn, "publish")
 
 	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是传输函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 为参数，产生输出得来的
 	require.Equal(t, rule.subTableIndexes[0], 1500)
 	require.Equal(t, rule.subTableIndexes[101], 1601) // 都同时加上 101
-	require.Equal(t, rule.tableToSlice[1500], 0)
-	require.Equal(t, rule.tableToSlice[1601], 1) // 加上 101 之后，进入下一个切片
+	require.Equal(t, rule.tableToSlice[1500], 0)      // 第一个范围的开头
+	require.Equal(t, rule.tableToSlice[1600], 0)      // 第一個范围的结尾
+	require.Equal(t, rule.tableToSlice[1601], 1)      // 加上 101 之后，进入下一个切片
+
+	require.Equal(t, len(rule.mycatDatabases), 0)
+	require.Equal(t, len(rule.mycatDatabaseToTableIndexMap), 0)
+}
+
+// TestNovelRouterModDateMonth 函式 🧚 是用来测试小說数据库的 date month 路由
+func TestNovelRouterModDateMonth(t *testing.T) {
+
+	// >>>>> >>>>> >>>>> >>>>> >>>>> 案例1
+	// 在第 1 台 Master 数据库有数据表 Book_0000
+	// 在第 2 台 Master 数据库有数据表 Book_0001
+
+	// 再建立 路由规则 设定模组
+	cfgRouter := models.Shard{
+		DB:          "novel",
+		Table:       "Book",
+		ParentTable: "",
+		Type:        "date_month",
+		Key:         "Publish",
+		// Locations:     []int{1, 1}, // 路由规则模式 date_year 不使用 Locations
+		Slices:        []string{"slice-0", "slice-1"},
+		DateRange:     []string{"150001-160012", "160101-170012"}, // 路由规则模式 DateRange 使用 date_month
+		TableRowLimit: 0,
+	}
+
+	// 直接产生路由规则
+	rule, err := parseRule(&cfgRouter)
+	require.Equal(t, err, nil)
+
+	// 检查目前的路由设定值
+	require.Equal(t, rule.ruleType, "date_month")
+	require.Equal(t, rule.db, "novel")
+	require.Equal(t, rule.table, "book")
+	require.Equal(t, rule.slices, []string{"slice-0", "slice-1"})
+	// require.Equal(t, rule.shard.(*DateMonthShard), rule.shard.(*DateMonthShard))
+	require.Equal(t, rule.shardingColumn, "publish")
+
+	// 下面的 rule.subTableIndexes 和 rule.tableToSlice 是传输函式 parseHashRuleSliceInfos 以 models.Shard 的 Locations 和 Slices 为参数，产生输出得来的
+	require.Equal(t, rule.subTableIndexes[0], 150001)
+	require.Equal(t, rule.subTableIndexes[100*12], 160001) // 都同时加上 101
+	require.Equal(t, rule.tableToSlice[150001], 0)         // 第一个范围的开头
+	require.Equal(t, rule.tableToSlice[150001], 0)         // 第一個范围的结尾
+	require.Equal(t, rule.tableToSlice[160101], 1)         // 加上 101 年之后，进入下一个切片
 
 	require.Equal(t, len(rule.mycatDatabases), 0)
 	require.Equal(t, len(rule.mycatDatabaseToTableIndexMap), 0)
